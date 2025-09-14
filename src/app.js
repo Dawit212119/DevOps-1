7f7d97b5479641fdc298702bfd79045a84dd27ba
Dawit Workeye <dawit.workye-ug@aau.edu.et>
2025-09-14 13:26:20 +0300

    Implement eslint and prettier

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
-app.get("/", (req, res) => {
-  res.send("yoo");
+app.get('/', (req, res) => {
+  res.send('yoo');
 });
 
 export default app;
